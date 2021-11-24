9473bc620d1fd1f44e2df373ca1b3da23d0eb1c3
Chen Riang Lam <79974338+chenriang1@users.noreply.github.com>
2021-11-24 19:48:31 +0800

    update correct code example

--- redis-spring-data-example/src/main/java/com/example/redis/lua/RedisLuaExample.java
+++ redis-spring-data-example/src/main/java/com/example/redis/lua/RedisLuaExample.java
@@ -6,39 +6,29 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.List;
 
-@Controller
+@Component
 public class RedisLuaExample {
 
     @Autowired
     RedisTemplate<String, String> redisTemplate;
 
-    @GetMapping
-    public void start() {
-        init();
-    }
-
     @SneakyThrows
+    @PostConstruct
     public void init() {
-        Resource resource = new ClassPathResource("accountSum.lua");
-        InputStream inputStream = resource.getInputStream();
-        String luaScriptString = new String(FileCopyUtils.copyToByteArray(inputStream), StandardCharsets.UTF_8);
         List<String> keys = Collections.singletonList("customer001");
         String[] args = new String[] {"savingAcc", "fdAcc", "currentAcc"};
-        String result = redisTemplate.execute(convertStringToScript(luaScriptString), keys, "savingAcc", "fdAcc", "currentAcc");
-        System.out.println("Total Amount : " + result);
+        Long result = redisTemplate.execute(getScript(), keys, args);
+        System.out.println("Total Amount : " + result.toString());
     }
 
-    private static RedisScript<String> convertStringToScript(String script) {
-        Resource scriptSource = new ClassPathResource(script);
-        return RedisScript.of(scriptSource, String.class);
+    private static RedisScript<Long> getScript() {
+        Resource scriptSource = new ClassPathResource("account_sum.lua");
+        return RedisScript.of(scriptSource, Long.class);
     }
 }
